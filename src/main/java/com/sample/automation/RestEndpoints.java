675b2a10df3c02a0ce6d6dbac35a2c152d151777
keerthi <keerthir211@gmail.om>
2017-09-19 19:48:03 -0500

    changed url endpoint

--- src/main/java/com/sample/automation/RestEndpoints.java
+++ src/main/java/com/sample/automation/RestEndpoints.java
@@ -2,7 +2,7 @@ package com.sample.automation;
 
 public interface RestEndpoints {
 
-    String POSTMAN_URL = "http://postman-echo.com/get/%s";
+    String POSTMAN_URL = "http://postman-echo.com/get";
 
 
 }
